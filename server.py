8e932e135d1b6b061cd8c87a959fe93a8778d121
Ivan <vin.ven@yandex.ru>
2021-09-20 16:29:59 +0300

    Проверка ошибок при инициализации сервера.

--- server.py
+++ server.py
@@ -1,18 +1,24 @@
+from os import error
 import socket
 
 
 sock = socket.socket(proto=socket.IPPROTO_TCP)
-sock.bind(("", 42069))
-sock.listen(1)
 
-conn, addr = sock.accept()
+try:
+    sock.bind(("192.168.11.148", 42069))
+    sock.listen(-1)
 
-print("Входящий адрес: ", addr)
+except Exception as err:
+    print(err)
 
-recv_string = conn.recv(1024).decode(encoding="utf-8")
-
-while recv_string != "exit":
-    print("Message: ", recv_string)
+else:
+    conn, addr = sock.accept()
+    print("Входящий адрес: ", addr)
     recv_string = conn.recv(1024).decode(encoding="utf-8")
 
-sock.close()
+    while recv_string != "exit":
+        print("Message: ", recv_string)
+        recv_string = conn.recv(1024).decode(encoding="utf-8")
+
+finally:
+    sock.close()
